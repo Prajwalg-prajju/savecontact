0814af4ac358c0f34aa5499fa42f688f7accdfa1
Prajwal G <prajwalgprajju12@gmail.com>
2025-12-04 18:04:03 +0530

    Prepare for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/savecontact/',   // ✅ THIS IS REQUIRED
+  base: '/savecontact/',  // ✅ must match your GitHub repo name
   plugins: [react()],
-  server: {
-    port: 5173
-  }
 })
